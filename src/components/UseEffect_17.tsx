8817c123347b642787b38802c8cb8101547800ef
PedroSmaxY <pedrohnovais@outlook.com>
2023-08-15 00:10:02 -0300

    16_hooks

--- src/components/UseEffect_17.tsx
+++ src/components/UseEffect_17.tsx
@@ -3,18 +3,38 @@ import { useState, useEffect } from "react";
 export default () => {
   const [valor, setValor] = useState<number>(() => 0);
 
+  const [nome, setNome] = useState<string>(() => "");
+
   const incremento = () => setValor((old) => old + 1);
 
+  const definirNome = () => setNome(() => "Pedro Henrique");
+
   useEffect(() => {
-    console.log("atualizado");
+    console.log("Executar apenas no arranque do componente");
   }, []);
+
+  useEffect(() => {
+    console.log("Executar apenas no arranque do VALOR");
+  }, [valor]);
+
+  useEffect(() => {
+    console.log("Executar apenas no arranque do NOME");
+  }, [nome]);
+
+  useEffect(() => {
+    console.log("Executar apenas no arranque do VALOR e do NOME");
+  }, [valor, nome]);
 
   return (
     <>
       <div>
         <h3>UseEffect 01</h3>
-        <button onClick={incremento}>Incremento</button>
         <h3>{valor}</h3>
+        <button onClick={incremento}>Incremento</button>
+        <div style={{ paddingTop: "20px" }}>
+          <h3>{nome}</h3>
+          <button onClick={definirNome}>Definir Nome</button>
+        </div>
       </div>
     </>
   );
